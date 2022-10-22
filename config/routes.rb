70478c1820ab52f22c2b0eb4e8afd33b33be28b0
adriannicolai <anlumbad@gmail.com>
2022-10-22 10:57:36 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :shop, only: [:index]
+
+  namespace :shop do
+
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "shop#index"
 end
