627635f30bae19ee1b812582ab851d9e34da8bf2
adriannicolai <anlumbad@gmail.com>
2022-10-23 18:01:36 +0800

    add helper if css and js is available

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+	before_action  :is_css_available, :is_js_available
+
+	private
+	def is_css_available
+		file = File.join(Rails.root, "app", "assets", "stylesheets", "#{params['controller']}_#{params['action']}.scss")
+		@is_css_available = File.exists?(file)
+	end
+
+	def is_js_available
+		file = File.join(Rails.root, "app", "assets", "javascripts", "#{params['controller']}_#{params['action']}.js")
+		@is_js_available = File.exists?(file)
+	end
 end
