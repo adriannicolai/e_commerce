1bbd5578e348376236014744caed86de81f0873f
adriannicolai <anlumbad@gmail.com>
2022-10-27 23:00:08 +0800

    add validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    STRING_ONLY_REGEX = /[@%^&!"\\\*\.,\-\:?\/\'=`{}()+_\]\|\[\><~;$#0-9]/
+
+    validates :first_name, presence: true, format: { with: STRING_ONLY_REGEX, message: "Special characters are not allowed", multiline: true }
+    validates :last_name, presence: true, format: { with: STRING_ONLY_REGEX, message: "Special characters are not allowed", multiline: true }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid Email Pattern" }, uniqueness: { case_sensitive: false }
 end
