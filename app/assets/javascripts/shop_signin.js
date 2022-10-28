b0f7bcc4bcbe84c9805c7e5d9945d00b1f48f27c
adriannicolai <anlumbad@gmail.com>
2022-10-29 07:50:05 +0800

    remove console log

--- app/assets/javascripts/shop_signin.js
+++ app/assets/javascripts/shop_signin.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
         let signin_form = $("#signin_form");
 
         $.post(signin_form.attr("action"), signin_form.serialize(), (signin_form_response) => {
-            console.log('signin_form_response :>> ', signin_form_response);
             if (signin_form_response.status) {
                 window.location.href = "/";
             }
