76b151bd57721ab514e95b5153e14d28d833556d
adriannicolai <anlumbad@gmail.com>
2022-10-28 21:03:29 +0800

    commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_141709) do
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "contact_number"
+    t.date "birthday"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
