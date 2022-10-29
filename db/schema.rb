d22192167610afa6ba56e60a23d78fd4da1bf49f
adriannicolai <anlumbad@gmail.com>
2022-10-29 08:04:56 +0800

    add user_level

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_141709) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_000018) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "contact_number"
     t.date "birthday"
+    t.string "user_level"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
