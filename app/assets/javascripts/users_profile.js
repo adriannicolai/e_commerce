ed91737b0b621c89ce870be0f568b3b81d0ee66b
adriannicolai <anlumbad@gmail.com>
2022-11-01 20:02:41 +0800

    comment codes

--- app/assets/javascripts/users_profile.js
+++ app/assets/javascripts/users_profile.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     /* On user click navigation */
-    $(".profile_redirect_button").on("click", function (e) {
-        e.preventDefault();
-        let href = $(this).attr("href");
+    // $(".profile_redirect_button").on("click", function (e) {
+    //     e.preventDefault();
+    //     let href = $(this).attr("href");
 
-        console.log('href :>> ', href);
-    });
+    //     console.log('href :>> ', href);
+    // });
 });
