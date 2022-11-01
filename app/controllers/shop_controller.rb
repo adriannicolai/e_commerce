edb9d748ec2d244eba5aaf0fa0b9956c98c5db3d
adriannicolai <anlumbad@gmail.com>
2022-11-01 10:50:53 +0800

    moved code

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,5 +1,5 @@
 class ShopController < ApplicationController
-  before_action :check_logged_in?, only: [:signin, :signup]
+  before_action :redirect_to_home_if_logged_in?, only: [:signin, :signup]
 
   def index
   end
@@ -8,11 +8,5 @@ class ShopController < ApplicationController
   end
 
   def signup
-  end
-
-  private
-  # Redirect to home page if logged in
-  def check_logged_in?
-    redirect_to "/" if session[:user].present?
   end
 end
