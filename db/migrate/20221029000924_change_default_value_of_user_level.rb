12b47ab71a2cea1742befbbf87e0778c2c40add2
adriannicolai <anlumbad@gmail.com>
2022-11-01 13:39:28 +0800

    rearrange the table

--- db/migrate/20221029000924_change_default_value_of_user_level.rb
+++ db/migrate/20221029000924_change_default_value_of_user_level.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultValueOfUserLevel < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :users, :user_level, from: nil, to: User.user_levels["user"], after: :password
+    change_column_default :users, :user_level, from: nil, to: User.user_levels["user"]
   end
 end
