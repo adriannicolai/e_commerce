17d5d62d802ae573820c505dd6fe39aee2130137
adriannicolai <anlumbad@gmail.com>
2022-11-03 19:25:43 +0800

    add default format

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update] do
       collection do
         post :authenticate_user
