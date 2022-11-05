d48c3feceddf33265d2830106e8a3038f59cdd59
adriannicolai <anlumbad@gmail.com>
2022-11-05 19:59:14 +0800

    add addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-    validates :first_name, :last_name, presence: true
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid Email Pattern" }, uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 8, message: "Password should be at least 8 characters" }
-    validates :password, presence: true, confirmation: true, on: :create
+	validates :first_name, :last_name, presence: true
+	validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid Email Pattern" }, uniqueness: { case_sensitive: false }
+	validates :password, presence: true, length: { minimum: 8, message: "Password should be at least 8 characters" }
+	validates :password, presence: true, confirmation: true, on: :create
 
-    # Validate first name and last name
-    validates_with UserRegistrationValidator
+	# Validate first name and last name
+	validates_with UserRegistrationValidator
 
-    enum user_level: %w[admin user]
+	enum user_level: %w[admin user]
+
+	has_many :addresses
 end
