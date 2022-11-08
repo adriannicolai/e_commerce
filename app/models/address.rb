a8910c5501059aef1329b8f11d6daa69de371843
adriannicolai <anlumbad@gmail.com>
2022-11-08 21:05:19 +0800

    add checker for number of addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :full_name, :phone_number, :province, :city, :detailed_address, presence: true
+  validates_with UserRegistrationValidator
+  validates_with NumberOfAddressValidator
 end
