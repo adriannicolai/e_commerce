615bdc5e8676ad3210d3d37bd4b709ad767b63c9
adriannicolai <anlumbad@gmail.com>
2022-11-08 20:45:35 +0800

    add create address controller

--- app/controllers/api/addresses_controller.rb
+++ app/controllers/api/addresses_controller.rb
@@ -1,4 +1,13 @@
 class Api::AddressesController < ApplicationController
   def create
+    @address = Address.new(address_params)
+    @address.assign_attributes(user_id: session[:user]["user_id"])
+
+    @error_object = @address.errors.messages unless @address.save
+  end
+
+  private
+  def address_params
+    params.require(:addresses).permit(:full_name, :phone_number, :province, :city, :detailed_address, :is_billing, :is_default)
   end
 end
