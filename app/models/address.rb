93560b53df87ad91ce9a227b28a86fa0a7e818f4
adriannicolai <anlumbad@gmail.com>
2022-11-09 22:01:48 +0800

    strip the values of the addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,10 @@ class Address < ApplicationRecord
   validates :full_name, :phone_number, :province, :city, :detailed_address, presence: true
   validates_with UserRegistrationValidator
   validates_with NumberOfAddressValidator
+
+  before_validation do
+    self.attributes.each do |key, value|
+      self.attributes[key] = value.to_s.strip if value.present?
+    end
+  end
 end
