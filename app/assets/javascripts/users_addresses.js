fba389058df3ab84bcc0fc171528f82395be3b41
adriannicolai <anlumbad@gmail.com>
2022-11-09 23:45:48 +0800

    add delete address

--- app/assets/javascripts/users_addresses.js
+++ app/assets/javascripts/users_addresses.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+	/* Create a new address */
 	$("#create_address_form").on("submit", function (e) {
 		e.preventDefault();
 
@@ -10,5 +11,30 @@ document.addEventListener("DOMContentLoaded", () => {
 				$("#add_address_modal").modal("hide");
 			}
 		});
+	});
+	/* Set the address route for deletion */
+	$(".delete_address").on("click", function (e) {
+		e.preventDefault();
+
+		$("#delete_address_modal").modal("show");
+
+		let address_id = $(this).closest(".address_card").data("address_id");
+		$("#delete_address_form").attr("action", `/api/addresses/${address_id}`);
+	});
+	/* Delete the address */
+	$("#confirm_delete_address_button").on("click", function() {
+		let delete_address_form = $("#delete_address_form");
+		$.ajax({
+			url: delete_address_form.attr("action"),
+			type: "DELETE",
+			data: delete_address_form.serialize(),
+			success: function(delete_address_response) {
+				if (delete_address_response.status){
+					$(`.address_${delete_address_response.address_id}`).remove();
+				}
+
+				$("#delete_address_modal").modal("hide");
+			}
+		})
 	});
 });
