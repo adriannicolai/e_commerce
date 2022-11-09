51d49f17f00fee652d5251c9edc916c72c9f7fa2
adriannicolai <anlumbad@gmail.com>
2022-11-09 20:50:26 +0800

    update validation for address number

--- app/validators/number_of_address_validator.rb
+++ app/validators/number_of_address_validator.rb
@@ -2,7 +2,7 @@ class NumberOfAddressValidator < ActiveModel::Validator
   def validate(record)
     check_number_of_address = check_number_of_address(record.user_id)
 
-    record.errors.add(:address_limit, "Cannot have more than 10 addresses") if check_number_of_address >= 10
+    record.errors.add(:address_limit, "Cannot have more than 12 addresses") if check_number_of_address >= 12
   end
 
   def check_number_of_address(user_id)
