742cfc165e2855a71ad8698537f02174b7a1fed3
adriannicolai <anlumbad@gmail.com>
2022-11-09 21:34:28 +0800

    add jquery before bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as bootstrap from "bootstrap"
 import "./src/jquery"
+import * as bootstrap from "bootstrap"
