ea834fc5c4b7121c6d068353d37d8fecbfd91b21
adriannicolai <anlumbad@gmail.com>
2022-11-11 18:57:28 +0800

    add js for updating addresss

--- app/assets/javascripts/users_addresses.js
+++ app/assets/javascripts/users_addresses.js
@@ -10,7 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		.on("click", "#confirm_delete_address_button", sumbitDeleteAddressForm)
 
 		/* Copy the value from the selected card to the modal for updating */
-		.on("click", ".address_card_footer .update_address", triggerUpdateAddressModal);
+		.on("click", ".address_card_footer .update_address", triggerUpdateAddressModal)
+
+		/* Submit the update address */
+		.on("submit", "#update_address_form", submitUpdateAddressForm)
 });
 
 function submitCreateAddressForm(e) {
@@ -55,10 +58,11 @@ function sumbitDeleteAddressForm() {
 }
 
 function triggerUpdateAddressModal() {
-	let address_id = $(this).closest(".address_card").data("address_id");
+	let address_id  = $(this).closest(".address_card").data("address_id");
+	let address_url = `/api/addresses/${address_id}`;
 
 	/* Fetch the address details and get the address details */
-	$.get(`/api/addresses/${address_id}`, function (fetch_address_response) {
+	$.get(address_url, function (fetch_address_response) {
 		if (fetch_address_response.status) {
 			/* Destructure the address details */
 			let { id, user_id, full_name, phone_number, province, city, detailed_address, is_billing, is_default } = fetch_address_response;
@@ -74,9 +78,23 @@ function triggerUpdateAddressModal() {
 			update_address_form.find("#update_detailed_address").val(detailed_address);
 			update_address_form.find("#update_is_billing").prop("checked", is_billing);
 			update_address_form.find("#update_is_default").prop("checked", is_default);
+			update_address_form.attr("action", address_url);
 
 			/* Show the address modal */
 			$("#update_address_modal").modal("show");
+		}
+	});
+}
+
+function submitUpdateAddressForm(e){
+	e.preventDefault();
+
+	let update_address_form = $(this);
+
+	$.post(update_address_form.attr("action"), update_address_form.serialize(), function(update_address_form_response){
+		if (update_address_form_response.status){
+			$("#update_address_modal").modal("hide");
+			$("#app_toast").toast("show");
 		}
 	});
 }
