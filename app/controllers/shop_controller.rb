759dbcf9cc102e10de3115b33269d97af46081d5
adriannicolai <anlumbad@gmail.com>
2022-11-19 21:34:57 +0800

    revert changes in shop

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,6 +1,5 @@
 class ShopController < ApplicationController
   before_action :redirect_to_home_if_logged_in?, only: [:signin, :signup]
-  before_action :is_user_admin, only: [:index]
 
   def index
   end
@@ -9,10 +8,5 @@ class ShopController < ApplicationController
   end
 
   def signup
-  end
-
-  private
-  def is_user_admin
-    redirect_to "/admin" if session[:user].present? && session[:user]["user_level"] === "admin"
   end
 end
