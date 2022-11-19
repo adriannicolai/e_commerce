1ebee951b502555533019d29397d2f3d750fafda
adriannicolai <anlumbad@gmail.com>
2022-11-19 20:17:52 +0800

    add redirect if user is admin

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,5 +1,6 @@
 class ShopController < ApplicationController
   before_action :redirect_to_home_if_logged_in?, only: [:signin, :signup]
+  before_action :is_user_admin, only: [:index]
 
   def index
   end
@@ -8,5 +9,10 @@ class ShopController < ApplicationController
   end
 
   def signup
+  end
+
+  private
+  def is_user_admin
+    redirect_to "/admin" if session[:user].present? && session[:user]["user_level"] === "admin"
   end
 end
