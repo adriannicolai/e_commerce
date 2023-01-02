c684553cf95e817cf5d646a57eb80fc3c1e72b85
adriannicolai <anlumbad@gmail.com>
2023-01-02 19:44:13 +0800

    update ngrok to use regex instead

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << ENV['NGROK_HOST'] if ENV['NGROK_HOST'].present?
+  config.hosts << /.*\.ngrok\.io/
 end
